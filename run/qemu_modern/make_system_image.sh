2b2228f7e243e5b6ec64fb28d3a586b0da72d8c9
SuperPrower <dya.eshshmai@gmail.com>
2018-05-23 14:00:59 +0300

    Fix and add checks for grub-mkrescue in make_system_image.sh script

--- run/qemu_modern/make_system_image.sh
+++ run/qemu_modern/make_system_image.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Save script directory before cd
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd $PHANTOM_HOME/run
+
+if ! type "grub-mkrescue" > /dev/null; then
+	echo "grub-mkrescue not found, please install grub package with your package manager or use external grub image (./run_phantom.sh --external-grub)"
+	exit
+fi
 
 echo "Making phantom_boot.img image, using content of fat folder"
-cp -r ./grub/* ./fat/
+cp -r "${SCRIPT_DIR}/grub_files/* ./fat/"
 sudo grub-mkrescue -o ./phantom_boot.img --modules="font gfxterm echo reboot usb_keyboard multiboot fat ls cat ext2 iso9660 reiserfs xfs part_sun part_gpt part_msdos" ./fat/
 
 sudo chown $USER phantom_boot.img
