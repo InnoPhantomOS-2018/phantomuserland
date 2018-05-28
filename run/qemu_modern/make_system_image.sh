c28b8b17296cd95d39275a2ecc6b82e53aa14172
SuperPrower <dya.eshshmai@gmail.com>
2018-05-28 13:37:25 +0300

    fix make_system_image script

--- run/qemu_modern/make_system_image.sh
+++ run/qemu_modern/make_system_image.sh
@@ -10,7 +10,7 @@ if ! type "grub-mkrescue" > /dev/null; then
 fi
 
 echo "Making phantom_boot.img image, using content of fat folder"
-cp -r "${SCRIPT_DIR}/grub_files/* ./fat/"
+cp -R ${SCRIPT_DIR}/grub_files/* ./fat/
 sudo grub-mkrescue -o ./phantom_boot.img --modules="font gfxterm echo reboot usb_keyboard multiboot fat ls cat ext2 iso9660 reiserfs xfs part_sun part_gpt part_msdos" ./fat/
 
 sudo chown $USER phantom_boot.img
